98f22cdc2dfc603b083307f42d67181dbb66933b
nguyenthaitan <nguyenthaitan9@gmail.com>
2021-11-17 18:50:38 +0700

    fix link mail

--- src/modules/feature/mail/mail.service.ts
+++ src/modules/feature/mail/mail.service.ts
@@ -3,9 +3,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MailService {
-  constructor(private mailerService: MailerService) {
-    this.sendMail('nguyenthaitan9@gmail.com','aaaaaaaaa', 'STUDENT', 'aaaaaa', 'ssssss', 'Tân Nguyễn', 'nguyentan@gmail.com', 'ABC')
-  }
+  constructor(private mailerService: MailerService) {}
 
   async sendMail(
     email: string,
@@ -23,7 +21,7 @@ export class MailService {
       subject: `Class Invitation - ${new Date(
         Date.now(),
       ).toLocaleDateString()}`,
-      html: `<p>${inviterName} (${inviterEmail}) want to invite you to join class "${classTitle}" as a "${role}".</p><br><a target="_blank" rel="noopener noreferrer" href="http://localhost:3000/classes/join?classId=${classId}&role=${role}&code=${classroomCode}${inviteCode}">Click here to accept.</a>`,
+      html: `<p>${inviterName} (${inviterEmail}) want to invite you to join class "${classTitle}" as a "${role}".</p><br><a target="_blank" rel="noopener noreferrer" href="https://nguyendonghuynhlang26123.github.io/classroom.frontend/#/classes/join?classId=${classId}&role=${role}&code=${classroomCode}${inviteCode}">Click here to accept.</a>`,
     });
   }
 }
